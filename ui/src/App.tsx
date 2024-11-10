aabde0ebb19a6fa31430f5d89b2665a3eef357d7
Enoch Chang <eeknoc@gmail.com>
2024-11-09 18:07:59 -0800

    v1 for frontend

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { SetStateAction, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setSearchTerm(event.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="video-container">
+          <video className="video-player"
+            src="https://www.w3schools.com/html/mov_bbb.mp4"
+            controls
+            muted={false}
+          />
+        </div>
+        <div className="playback-container">
+          <div style={{ margin: '20px' }}>
+            <input
+              type="text"
+              placeholder="Search for tags..."
+              value={searchTerm}
+              onChange={handleInputChange}
+              style={{
+                padding: '9px',
+                width: '250px',
+                borderRadius: '5px',
+                border: '1px solid #ccc',
+              }}
+            />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
